89677ff81c307a003a595a1639d101727d64a89f
kmrtmk03 <krmmmmmmrk@gmail.com>
2019-09-13 23:24:48 +0900

    リロードをしても404がでないように

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
-// server.js
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+ var express = require('express');
+ var app = express();
+ var history = require('connect-history-api-fallback');
+
+ app.use(history());
+
+ app.use(express.static(__dirname + "/dist"));
+
+ app.set('port', (process.env.PORT || 8080));
+ app.listen(app.get('port'), () => {
+ console.log('Derp is running at localhost: 8080');
+ }); 
+
+
+//  // server.js
+// var express = require('express');
+// var path = require('path');
+// var serveStatic = require('serve-static');
+// app = express();
+// app.use(serveStatic(__dirname + "/dist"));
+// var port = process.env.PORT || 5000;
+// app.listen(port);
+// console.log('server started '+ port);
